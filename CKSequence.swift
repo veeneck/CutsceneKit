253b668881025593726622b51aa30e9fc21b1a03
Ryan Campbell <ryancampbell@Ryans-iMac-2.local>
2015-11-17 17:05:45 -0500

    Initial commit

---
+++ CKSequence.swift
@@ -0,0 +1,17 @@
+//
+//  CSSequence.swift
+//  CutsceneKit
+//
+//  Created by Ryan Campbell on 11/17/15.
+//  Copyright © 2015 Phalanx Studios. All rights reserved.
+//
+
+import SpriteKit
+
+public class CKSequence {
+    
+    public init() {
+        
+    }
+    
+}
---
+++ CutsceneKit.h
@@ -0,0 +1,19 @@
+//
+//  CutsceneKit.h
+//  CutsceneKit
+//
+//  Created by Ryan Campbell on 11/17/15.
+//  Copyright © 2015 Phalanx Studios. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CutsceneKit.
+FOUNDATION_EXPORT double CutsceneKitVersionNumber;
+
+//! Project version string for CutsceneKit.
+FOUNDATION_EXPORT const unsigned char CutsceneKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CutsceneKit/PublicHeader.h>
+
+
